ece911e00e4997f13583dbbc87d044d30e787f7e
issenn <issenn@hellotalk.com>
2019-03-21 17:58:14 +0800

    Initial commit

--- vars/binaryNode.groovy
+++ vars/binaryNode.groovy
@@ -5,25 +5,9 @@ def call(Map parameters = [:], Closure body={}) {
     def defaultLabel = buildId('binary')
     def _label = parameters.get('label', defaultLabel)
 
-    pipeline {
-        agent none
-
-        stages {
-            stage('Test') {
-                agent {
-                    node {
-                        label "${_label}"
-                        customWorkspace "workspace/${JOB_NAME.replace('%2F', '/')}"
-                    }
-                }
-                when {
-                    beforeAgent true
-                    branch "master"
-                }
-                steps {
-                    sh "echo test."
-                }
-            }
+    binaryTemplate(parameters) {
+        node(_label) {
+            body()
         }
     }
 }
