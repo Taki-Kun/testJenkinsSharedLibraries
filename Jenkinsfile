ab31d6e918d1a8f5357e4f12aef446f2e692eb50
issenn <issenn@hellotalk.com>
2019-03-21 09:42:49 +0800

    Initial commit

---
+++ Jenkinsfile
@@ -0,0 +1,15 @@
+#!/usr/bin/env groovy
+
+import io.fabric8.Fabric8Commands
+
+
+// @Library('github.com/docker/jenkins-pipeline-scripts') _
+// @Library('github.com/fabric8io/fabric8-pipeline-library@master')
+@Library('github.com/Taki-Kun/testJenkinsSharedLibraries@master') _
+def dummy
+clientsNode {
+    ws ('pipelines'){
+        git 'https://github.com/fabric8io/fabric8-pipeline-library.git'
+    }
+}
+
---
+++ vars/clientsNode.groovy
@@ -0,0 +1,29 @@
+#!/usr/bin/env groovy
+
+def call(Map parameters = [:], body) {
+
+    // def defaultLabel = buildId('clients')
+    // def label = parameters.get('label', defaultLabel)
+
+    pipeline {
+        agent none
+
+        stages {
+            stage('Test') {
+                agent {
+                    node {
+                        label 'master'
+                        customWorkspace "workspace/${JOB_NAME.replace('%2F', '/')}"
+                    }
+                }
+                when {
+                    beforeAgent true
+                    branch "develop"
+                }
+                steps {
+                    sh "echo test."
+                }
+            }
+        }
+    }
+}
