caef0d067c30cf05c262d21222e45b75850c1827
issenn <issenn@hellotalk.com>
2019-03-21 19:46:23 +0800

    Initial commit

--- vars/checkoutGeneralSCM.groovy
+++ vars/checkoutGeneralSCM.groovy
@@ -6,6 +6,11 @@ def call(Map parameters = [:]) {
     echo _browser
     def _url = parameters.get('url')
     echo scm.userRemoteConfigs[0]['credentialsId']
+    echo scm.userRemoteConfigs[0]['credentialsId'].size()
+    echo scm.userRemoteConfigs[0].size()
+    // for (def index = 0; index < echo scm.userRemoteConfigs[0].size(); index++) {
+    //     nameParts[index] = URLDecoder.decode(nameParts[index], 'UTF-8')
+    // }
 
     checkout changelog: true, poll: true, scm: [
         $class: 'GitSCM',
